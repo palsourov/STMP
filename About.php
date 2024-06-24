13376f49cb9db9d2446602f6adfad2949c1394c8
mowlyray <mowlyray421@gmail.com>
2024-06-24 23:29:44 +0600

    update about.php

--- About.php
+++ About.php
@@ -1,13 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>About Us</h1>
-    
-   
-</body>
-</html>
+<?php  
+     require_once("Header.php");
+?>
+
+
+
+
+ <!--banner slider start-->
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+
+          <div class="slider">
+          <img src="https://t3.ftcdn.net/jpg/07/94/56/88/240_F_794568822_cTjKycU1DRATl19nBfIGPKcBviHllUFD.jpg" class="d-block w-100" alt="...">
+
+          </div>
+          
+
+            <div class="carousel-caption">
+                <h1 data-aos="fade-up" data-aos-offset="250">ABOUT-US</h1>
+                <hr class="h" >
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://t4.ftcdn.net/jpg/05/83/90/17/240_F_583901767_l0ZxyLupe4NHbNnLeEuTK35AvC2Ccu26.jpg" class="d-block w-100" alt="...">
+
+            <div class="carousel-caption">
+                <h1  >ABOUT-US</h1>
+                <hr class="h" >
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="https://t4.ftcdn.net/jpg/07/02/41/65/240_F_702416526_aKPTOYvulTw48bEEH8CxHlabQyqvrnId.jpg" class="d-block w-100" alt="...">
+
+            <div class="carousel-caption  ">
+                <h1 >ABOUT-US</h1>
+                <hr class="h" >
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <!--banner slider end--!
+
+
+
+
+      
+<?php  
+     require_once("Footer.php");
+?>
--- Header.php
+++ Header.php
@@ -13,7 +13,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css"/>
+
+    <link rel="stylesheet" href="./style/style.css">
+
   </head>
   <body>
 <!-- NavBar start -->
