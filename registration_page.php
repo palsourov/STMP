fc45322c5684e615fc4b44b0a13cb7fffe0891b8
MughniRayhan <tishtisha13@gmail.com>
2024-06-25 16:59:09 +0600

    update style on registration page

--- registration_page.php
+++ registration_page.php
@@ -2,33 +2,43 @@
    require_once("Header.php")
 ?>
 
-<div class="loginform">
+ <div class="loginform">
     <div class="container">
-        <h2>Registration Form</h2>
+        <h3>Registration Form</h3>
         <form id="registrationForm">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" placeholder="Enter your username" required>
+           <div class="formInput">
+           <label for="username">Username : </label>
+           <input  type="text" id="username" name="username" placeholder="Enter your username" required>
+           </div>
             
-            <label for="email">Email</label>
+            <div class="formInput">
+            <label for="email">Email : </label>
             <input type="email" id="email" name="email" placeholder="Enter your email" required>
             
-            <label for="password">Password</label>
+            </div>
+           <div class="formInput">
+           <label for="password">Password : </label>
             <input type="password" id="password" name="password" placeholder="Enter your password" required>
-            
-            <label for="confirmPassword">Confirm Password</label>
+           </div>
+
+            <div class="formInput">
+            <label for="confirmPassword">Confirm Password : </label>
             <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm your password" required>
+            </div>
             
-            <label for="phone">Phone Number</label>
-            <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" pattern="[0-9]{10}" required>
+           <div class="formInput">
+           <label for="phone">Phone Number : </label>
+           <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" pattern="[0-9]{10}" required>
+           </div>
             
-            <input type="submit" value="Register">
+         <input  class="register" type="submit" value="Register">
             
             <div class="error" id="errorMessage"></div>
         </form>
     </div>
-    </div>
-
+    </div> 
     
+     
 <?php 
    require_once("Footer.php")
 ?>
